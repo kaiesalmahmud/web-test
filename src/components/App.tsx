7148ad9208a662785a6bf45e05ec1dcf39339771
kaiesalmahmud <kaies.rmedu03@gmail.com>
2023-07-25 16:03:34 +0600

    photo test

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,7 +6,7 @@ export default function App(): any {
     <section className='h-screen flex flex-col justify-center items-center bg-black/95 text-white'>
       <img
         className='h-screen object-contain w-full'
-        src='https://images.unsplash.com/photo-1636690498207-d7b393423b9a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80'
+        src='https://avatars.githubusercontent.com/u/59995405?v=4?s=400'
         alt='ai'
       />
       <Heading className='absolute'>Website Coming Soon</Heading>
